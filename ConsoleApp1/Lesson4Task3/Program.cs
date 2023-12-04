eebf2467d47949c5d4641c88639f7db0ce7af02d
Masha Asadchaya <maryia.asadchaya@smartr365.com>
2023-12-04 21:14:50 +0000

    Lesson 4 Task 3 Generate an array of random numbers

---
+++ ConsoleApp1/Lesson4Task3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace Lesson4Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = new int[5];
+            Random rand = new Random();
+
+            Label:
+            for (int i = 0; i < myArray.Length; i++) {
+                myArray[i] = rand.Next(10);
+            }
+
+            Console.WriteLine(String.Join(", ", myArray));
+
+            foreach (int item in myArray)
+            {
+                if (item == 0)
+                {
+                    Console.WriteLine("One of the numbers equal 0, generating new array");
+                    goto Label;
+                }
+            }
+        }
+    }
+}
