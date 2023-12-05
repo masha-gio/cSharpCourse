0a88285957b17a077c752aefcf39a4605b32818e
Masha Asadchaya <maryia.asadchaya@smartr365.com>
2023-12-05 12:57:57 +0000

    HW lesson 5 Task 2 Add a function that output the timetable for the entered number by the max number entered Rename programs

---
+++ ConsoleApp1/Lesson5Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Lesson5Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            var isNumber = int.TryParse(Console.ReadLine(), out int number);
+            Console.WriteLine("PLease enter maximum value");
+            var isMaxValue = int.TryParse(Console.ReadLine(), out int maxValue);
+            if(isNumber && isMaxValue) {
+            PrintTimeTable(number, maxValue);
+            }
+            else
+            {
+                Console.WriteLine("One of the entries is not a valid number");
+            }
+            
+        }
+
+        static void PrintTimeTable(int number, int maxValue)
+        {
+            Console.WriteLine($"Timetable for number {number}:");
+            for (int i = 0; i <= maxValue; i++) {
+                int result = i * number;
+                Console.WriteLine($"{ number} * { i} = { result}");
+            }
+        }
+    }
+}
