9d86047e0d9b98f8b769de17f713100e6b54fc79
Masha Asadchaya <maryia.asadchaya@smartr365.com>
2023-12-12 22:15:46 +0000

    HW Lesson 7 Task 1 Add ArrayWorker class library with method to invert array

---
+++ ConsoleApp1/Lesson7Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿using ArrayWorkerLibrary;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] currentArray = new[] { 1, 2, 3, 4 };
+        Console.WriteLine("Original array:");
+        PrintArray(currentArray);
+        ArrayWorker array = new ArrayWorker(currentArray);
+        int[] invertedArray = array.InvertArray(currentArray);
+        Console.WriteLine("Inverted array:");
+        PrintArray(invertedArray);
+    }
+
+    static void PrintArray(int[] array)
+    {
+        foreach (int i in array)
+        {
+            Console.WriteLine(i + " ");
+        }
+    }
+}
