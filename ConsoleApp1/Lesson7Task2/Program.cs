ae49a5271c9b17b47c7bd0e02b2aacc59c59c470
Masha Asadchaya <maryia.asadchaya@smartr365.com>
2023-12-12 22:52:42 +0000

    HW Lesson 7 Task 2 Add Cone class that has two methods to calculate Base and Total area

---
+++ ConsoleApp1/Lesson7Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿using Lesson7Task2;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Cone myCone = new Cone(3, 4);
+        double myBaseSurfaceArea = myCone.BaseSurfaceArea();
+        Console.WriteLine($"Base surfase area is {myBaseSurfaceArea}");
+        double myTotalSurfaceArea = myCone.TotalSurfaceArea();
+        Console.WriteLine($"Total surface area is {myTotalSurfaceArea}");
+    }
+}
