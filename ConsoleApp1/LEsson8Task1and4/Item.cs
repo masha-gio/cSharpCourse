4b183bab258943e70215b5e74d563fbd2b00ff1b
Masha Asadchaya <maryia.asadchaya@smartr365.com>
2023-12-18 14:39:09 +0000

    HW Lesson 8 Tasks 1 and 4 Create a Bag class that will check if the bag is opened, add item to the opened bag if the bag is not full, remove the item from the opened bag by index.

---
+++ ConsoleApp1/LEsson8Task1and4/Item.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson8Task1and4
+{
+    internal class Item
+    {
+        private string Name { get; set; }
+
+        public Item()
+        {
+            Name = "unknown";
+        }
+
+        public void SetName(string newName)
+        {
+            Name = newName;
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+    }
+}
---
+++ ConsoleApp1/LEsson8Task1and4/Program.cs
@@ -0,0 +1,30 @@
+﻿using Lesson8Task1and4;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Bag myBag = new Bag();
+
+        //open the bag
+        myBag.ToggleBag();
+
+        Item myItem = new Item();
+        myItem.SetName("Keys");
+
+        //add item to the bag
+        myBag.AddItem(myItem);
+
+        //close the bag
+        myBag.ToggleBag();
+
+        //add item to closed bag
+        myBag.AddItem(new Item());
+
+        //open the bag
+        myBag.ToggleBag();
+
+        Item removedItem = myBag.RemoveItem(0);
+
+    }
+}
