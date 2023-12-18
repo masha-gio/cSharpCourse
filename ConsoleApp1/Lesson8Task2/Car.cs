cb5abd2f18ef6c9a323cf44015990c9b632da0a8
Masha Asadchaya <maryia.asadchaya@smartr365.com>
2023-12-18 13:36:59 +0000

    Lesson 8 Task 2 Implement class Engine, abstract class Transport and derived classes Car and Plane

---
+++ ConsoleApp1/Lesson8Task2/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson8Task2
+{
+    internal class Car : Transport
+    {
+        public override Engine TransportEngine { get; } = new Engine();
+
+        public override void Move()
+        {
+            Console.WriteLine("Car riding");
+        }
+    }
+}
---
+++ ConsoleApp1/Lesson8Task2/Engine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson8Task2
+{
+    internal class Engine
+    {
+        public void Start()
+        {
+            Console.WriteLine("Engine has started");
+        }
+    }
+}
