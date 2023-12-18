b0d27ee5d1567da79b07439e447e449ea6281989
Masha Asadchaya <maryia.asadchaya@smartr365.com>
2023-12-18 13:49:09 +0000

    HW Lesson 8 Task 3 Add class Array with extension method that finds max value in array

---
+++ ConsoleApp1/Lesson8Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿using Lesson8Task3;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] myArray = {1, 3, 5, 8, 2, 16 };
+        int maxNumber = myArray.FindMaxValue();
+        Console.WriteLine(maxNumber);
+    }
+}
