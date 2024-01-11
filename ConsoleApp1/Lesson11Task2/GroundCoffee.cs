5883b723d2ece9f300dbebb368f7c4c7c22dfd16
Masha Asadchaya <maryia.asadchaya@smartr365.com>
2024-01-11 10:01:26 +0000

    lesson11

--- ConsoleApp1/Lesson11Task2/GroundCoffee.cs
+++ ConsoleApp1/Lesson11Task2/GroundCoffee.cs
@@ -8,5 +8,39 @@ namespace Lesson11Task2
 {
     internal class GroundCoffee
     {
+        public int Volume { get; set; }
+        public CoffeeStrength Strength { get; set; }
+
+        public GroundCoffee(int volume, CoffeeStrength strength)
+        {
+            Volume = volume;
+            Strength = strength;
+        }
+
+        public static Coffee operator +(Water water, GroundCoffee groundCoffee)
+        {
+            CoffeeStrength coffeeStrength = CalculateCoffeeStrength(water, groundCoffee);
+
+            int coffeeVolume = Math.Min(water.Volume, groundCoffee.Volume);
+
+            return new Coffee(coffeeVolume, coffeeStrength);
+        }
+
+        private static CoffeeStrength CalculateCoffeeStrength(Water water, GroundCoffee groundCoffee)
+        {
+
+            if (water.Temperature > 90 && groundCoffee.Strength == CoffeeStrength.Strong)
+            {
+                return CoffeeStrength.Strong;
+            }
+            else if (water.Temperature > 80 && groundCoffee.Strength == CoffeeStrength.Medium)
+            {
+                return CoffeeStrength.Medium;
+            }
+            else
+            {
+                return CoffeeStrength.Light;
+            }
+        }
     }
 }
