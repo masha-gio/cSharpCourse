b1a10a856e1365770ce5fab29013a7a70aa7ce8b
Masha Asadchaya <maryia.asadchaya@smartr365.com>
2024-01-11 09:57:19 +0000

    Lesson 11 Pandas task Coffee task

---
+++ ConsoleApp1/Lesson11Task1/Pandas.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson11Task1
+{
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+
+    public enum Colour
+    {
+        WhiteBlack,
+        White,
+        Black
+    }
+    internal class Panda : IComparable<Panda>
+    {
+        public Gender Gender { get; set; }
+        public Colour Colour { get; set; }
+        public int HP { get; set; }
+        public int Attack { get; set; }
+        public int Defence { get; set; }
+        public string Name { get; set; }
+
+        public Panda(Gender gender, Colour colour, int hp, int attack, int defence, string name)
+        {
+            Gender = gender;
+            Colour = colour;
+            HP = hp;
+            Attack = attack;                
+            Defence = defence;
+            Name = name;
+        }
+
+        public static Panda operator +(Panda parent1, Panda parent2)
+        {
+            if (parent1.Gender != parent2.Gender)
+            {
+                Colour newColour = parent1.Gender == Gender.Female ? parent1.Colour : parent2.Colour;
+                return new Panda(parent1.Gender, newColour, 100, 10, 5, "Baby panda");
+            }
+
+            throw new InvalidOperationException("Can not breed pandas of the same gender");
+        }
+         public static Panda operator -(Panda attacked, Panda attacker)
+        {
+            int damage = attacker.Attack - attacked.Defence;
+            attacked.HP = Math.Max(attacker.HP - damage, 1);
+            return attacked;
+        }
+
+        public int CompareTo(Panda other)
+        {
+            if (Colour != other.Colour)
+            {
+                return Colour.CompareTo(other.Colour);
+            }
+            else
+            {
+                return HP.CompareTo(other.HP);
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}: Gender: {Gender}, Color: {Colour}, HP: {HP}, Attack: {Attack}, Defence: {Defence}";
+        }
+
+    }
+}
---
+++ ConsoleApp1/Lesson11Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Lesson11Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Panda panda1 = new Panda(Gender.Female, Colour.WhiteBlack, 80, 15, 8, "Mai");
+            Panda panda2 = new Panda(Gender.Male, Colour.Black, 90, 12, 6, "Kai");
+
+            try
+            {
+                // Mate two pandas
+                Panda babyPanda = panda1 + panda2;
+                Console.WriteLine($"Baby Panda: {babyPanda}");
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+
+            // Attack one panda to reduce its HP
+            Panda attackedPanda = panda2 - panda1;
+
+            Console.WriteLine($"Attacked Panda: {attackedPanda}");
+        }
+    }
+}
