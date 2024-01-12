3d9763d4297790a25afbeb25652c3c7c33b4092f
Masha Asadchaya <maryia.asadchaya@smartr365.com>
2024-01-12 22:42:21 +0000

    Lesson 12Task 5 Mobile network class/ generics

---
+++ ConsoleApp1/Lesson12Task5/MobileNetwork.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson12Task5
+{
+    internal class MobileNetwork
+    {
+        public string Name { get; set; }
+
+        public MobileNetwork(string name) {
+            Name = name;
+        }
+    }
+}
---
+++ ConsoleApp1/Lesson12Task5/MobilePhone.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson12Task5
+{
+    internal class MobilePhone<T> where T : MobileNetwork
+    {
+        public T Network { get; set; }
+
+        public MobilePhone(T network) { 
+        Network = network;
+        }
+
+        public void DisplayNetworkInfo()
+        {
+            Console.WriteLine($"Mobile phone network is {Network}");
+        }
+    }
+}
