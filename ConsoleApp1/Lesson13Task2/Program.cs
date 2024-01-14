ccb5772a1052878f09a15eb5f0093ac56436ee2e
Masha Asadchaya <maryia.asadchaya@smartr365.com>
2024-01-14 20:26:49 +0000

    Lesson 13 Task 2 Create Collection<T>

---
+++ ConsoleApp1/Lesson13Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Lesson13Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomList<SampleClass> customList = new CustomList<SampleClass>();
+
+            customList.AddAndSort(new SampleClass(3));
+            customList.AddAndSort(new SampleClass(1));
+            customList.AddAndSort(new SampleClass(2));
+
+            customList.AddAndSort(new SampleClass(4));
+        }
+    }
+}
