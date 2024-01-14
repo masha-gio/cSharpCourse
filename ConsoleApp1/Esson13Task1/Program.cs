001125c635765b4a3015eb3980e815b504f00b95
Masha Asadchaya <maryia.asadchaya@smartr365.com>
2024-01-14 20:03:40 +0000

    Lesson 13 Task 1 Generate a list of random words from collection Filter words from the list on the length

---
+++ ConsoleApp1/Esson13Task1/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Lesson13Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> myDictionary = new Dictionary<int, string> {
+            { 1, "apple" },
+            { 2, "banana" },
+            { 3, "orange" },
+            { 4, "grape" },
+            { 5, "melon" },
+            { 6, "cherry" },
+            { 7, "kiwi" },
+            { 8, "pear" },
+            { 9, "plum" },
+            { 10, "peach" } 
+            }; 
+            Words words = new Words(myDictionary);
+
+            string[] randomWords = words.GenerateRandomWords(3);
+            
+            for(int i = 0; i < randomWords.Length; i++)
+            {
+                Console.WriteLine(randomWords[i]);
+            }
+
+            foreach(string word in words.FilterWordsByLength(randomWords, 5)) { 
+            Console.WriteLine(word);
+            }            
+        }
+    }
+}
---
+++ ConsoleApp1/Esson13Task1/Words.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson13Task1
+{
+    internal class Words
+    {
+        private Dictionary<int, string> dictionary;
+
+        public Words(Dictionary<int, string> customDictionary)
+        {
+            dictionary = customDictionary;
+        }
+
+        public string[] GenerateRandomWords(int count)
+        {
+            Random random = new Random();
+            List<string> words = new List<string>();
+            for (int i = 0; i < count; i++)
+            {
+                int randomKey = random.Next(1, 11);
+                words.Add(dictionary[randomKey]);
+            }
+            return words.ToArray();
+        }
+
+        public IEnumerable<string> FilterWordsByLength(string[] words, int minLength)
+        {
+            foreach (string word in words)
+            {
+                if (word.Length < minLength)
+                {
+                    yield return word;
+                }
+            }
+        }
+    }
+}
