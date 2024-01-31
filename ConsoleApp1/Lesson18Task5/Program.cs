6e6ec8fbbb669ed181e501bf0596a278737dd2b1
Masha Asadchaya <maryia.asadchaya@smartr365.com>
2024-01-31 20:24:49 +0000

    Lesson18 Threads

---
+++ ConsoleApp1/Lesson18Task5/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lesson18Task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
